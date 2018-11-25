aebc5d0625259405acbf9046b4a36ed725499896
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2018-11-25 14:16:24 +0900

    update_wiki_page

--- BacklogApiProcessing/utils/wiki_util.py
+++ BacklogApiProcessing/utils/wiki_util.py
@@ -14,7 +14,7 @@ class WikiUtil():
     def add_actual_hours_to_content(self, wiki_content_table, actual_hours_list, year_month):
         wiki_content_row_list = wiki_content_table.split('\r\n')
         #wiki_content_row_list = wiki_content_row_list[2: len(wiki_content_row_list) + 1]
-        add_last_row_list = [ year_month," ---- "]
+        add_last_row_list = [year_month, " ---- "]
         add_last_row_list.extend(list(map(str, actual_hours_list)))
         for i in range(len(wiki_content_row_list)):
             #wiki_content_row_list[i] = wiki_content_row_list[i] + " " + add_last_row_list[i] + " |"
@@ -23,5 +23,5 @@ class WikiUtil():
         return '\r\n'.join(wiki_content_row_list)
 
     def update_wiki_page(self, wiki_id, name, content, mail_notify):
-        params = {"name" : name, "content" : content, "mailNotify" : str(mail_notify)}
+        params = {"name" : name, "content" : content, "mailNotify" : str(mail_notify).lower().strip()}
         self.client.update_wiki(wiki_id, params)
